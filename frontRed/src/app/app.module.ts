96f6a4a6f104f9972e84fa0251f245e7f65580cb
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-02-19 08:36:13 -0500

    corregir librerias

--- frontRed/src/app/app.module.ts
+++ frontRed/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UpdatePersonComponent } from './components/update-person/update-person.component';
 import {  RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule ,NO_ERRORS_SCHEMA} from '@angular/core';
@@ -20,11 +21,7 @@ import { MatLinkPreviewModule } from '@angular-material-extensions/link-preview'
 import { EmbedVideo } from 'ngx-embed-video';
 import { Select2Module } from 'ng2-select2';
 import { MeetPeopleComponent } from './components/meet-people/meet-people.component';
-<<<<<<< HEAD
 import { TrenPubsComponent } from './components/tren-pubs/tren-pubs.component';
-=======
-import { UpdatePersonComponent } from './components/update-person/update-person.component';
->>>>>>> 2bc3b3b8ba6dcf3d87c6fc4d3cbcd384bfdae78c
 
 
 @NgModule({
@@ -38,11 +35,7 @@ import { UpdatePersonComponent } from './components/update-person/update-person.
     HomePublicationComponent,
     InsertPublicationComponent,
     MeetPeopleComponent,
-<<<<<<< HEAD
     TrenPubsComponent
-=======
-    UpdatePersonComponent
->>>>>>> 2bc3b3b8ba6dcf3d87c6fc4d3cbcd384bfdae78c
   ],
   
   imports: [
